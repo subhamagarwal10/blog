0dba6b3c92dffa7c57f3a83fdffafe5bdfa4d71c
subham <sasubhamagarwal10@gmail.com>
2018-03-15 21:59:55 +0530

    My first commit
    my first commit through github desktop

--- my_blog/views.py
+++ my_blog/views.py
@@ -7,10 +7,12 @@ AT = Airtable('appDpdZWtRgNqFRW7','blog','keyxFd1DgOAHee1jv')
 def home_page(request):
 	user_query = str(request.GET.get('search_post', ''))
 	search_result = AT.get_all(formula=("FIND('" + user_query.lower() + "', LOWER({Post_name}))"))
-	stuff_for_frontend = {'search_result':search_result}
+	all_results = AT.get_all()
+	stuff_for_frontend = {'search_result': search_result, 'all_results': all_results}
 	return render(request, "blog/blog_stuff.html", stuff_for_frontend)
 	
 def pages(request, post_id):
 	detail_result = AT.get(post_id)
-	stuff_for_frontend1 = {'detail_result1':detail_result}
+	all_results = AT.get_all()
+	stuff_for_frontend1 = {'detail_result1': detail_result, 'all_results': all_results}
 	return render(request, "blog/pages.html", stuff_for_frontend1)
